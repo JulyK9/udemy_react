cfc586f336c534bde4f910e0fb39a6a2e16081e7
JKKIM <jkesonjk@gmail.com>
2023-01-21 11:24:23 +0900

    #1 feat: dispatch 함수로 action 전달하기

--- redux-basic-project/src/components/Counter.js
+++ redux-basic-project/src/components/Counter.js
@@ -1,8 +1,17 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux"; // 리덕스 저장소의 데이터에 접근하기 위한 메서드 소환
+import { useSelector, useDispatch } from "react-redux"; // 리덕스 저장소의 데이터에 접근하기 위한 useSelector
 
 const Counter = () => {
+  const dispatch = useDispatch(); // 저장소(store)에 action을 보내는 함수
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" }); // dispatch 함수에 인자로 전달하는 객체는 type 속성을 명시해서 어떤 action 인지 알 수 있게
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -10,6 +19,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
