e5c3c8975c67a2d432f0d43de82bf64e74e28ae6
JKKIM <jkesonjk@gmail.com>
2023-02-10 22:32:22 +0900

    #3 feat: 카트 ui 토글 구현

--- redux-advanced/src/store/index.js
+++ redux-advanced/src/store/index.js
@@ -3,7 +3,7 @@ import uiSlice from "./ui-slice";
 
 const store = configureStore({
   reducer: {
-    ui: uiSlice.reducer, // reducer는 uiSlice에 의해 만들어짐
+    ui: uiSlice.reducer, // reducer는 uiSlice에 의해 만들어짐, selector로 store에서 값에 접근할 때 속성값
   },
 });
 
