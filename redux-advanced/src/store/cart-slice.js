411f021591d69db902fe70eff48fc499f0b272d7
JKKIM <jkesonjk@gmail.com>
2023-02-10 22:44:07 +0900

    #3 feat: 카트 아이템 slice 구현, 아이템 추가 리듀서 작성

---
+++ redux-advanced/src/store/cart-slice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+createSlice({
+  name: "cart",
+  initialState: {
+    // 상태 슬라이스의 구조를 잘 생각해보는 게 중요해보임
+    // 상품 항목(배열), 총 수량의 합, 총 가격
+    items: [],
+    totalQuantity: 0,
+    // totalPrice: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      // action에 데이터가 추가되어야 하므로 action 을 매개변수로 받아서 사용
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      // 기존에 없는 아이템인 경우
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          title: newItem.title,
+        });
+      } else {
+        // 기존에 있는 아이템인 경우
+        existingItem.quantity++; // 수량 증가시키고
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price; // 총가격 증가
+      }
+    },
+    removeItemToCar() {},
+  },
+});
