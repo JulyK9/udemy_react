9757d21e6a68f993d7d46d46cdef493292b7ca58
JKKIM <jkesonjk@gmail.com>
2023-02-13 00:34:36 +0900

    #3 feat: 리듀서는 유지하면서 컴포넌트에서 useEffect를 통해 서버에서 상태를 관리핧 수 있도록 로직 구현

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -2,9 +2,19 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const toggleCart = useSelector((state) => state.ui.cartIsVisible);
+
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://nextjs-course-e6529-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
